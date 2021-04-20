7d921909314876ae668c03e599b74051624145e4
Pablo Waehner <waehnermp@gmail.com>
2021-04-20 15:28:36 +0200

    push to github

--- register.php
+++ register.php
@@ -2,6 +2,7 @@
 include("includes/config.php");
 include("includes/classes/Account.php");
 include("includes/classes/Constants.php");
+// require_once 'db/conn.php';
 
 $account = new Account($con);
 
@@ -51,11 +52,13 @@ function getInputValue($name)
                     <p>
                         <?php echo $account->getError(Constants::$loginFailed); ?>
                         <label for="loginUsername">Username</label>
-                        <input id="loginUsername" name="loginUsername" type="text" placeholder="name" value="<?php getInputValue('loginUsername') ?>"  required>
+                        <input id="loginUsername" name="loginUsername" type="text" placeholder="name"
+                            value="<?php getInputValue('loginUsername') ?>" required>
                     </p>
                     <p>
                         <label for="loginPassword">Password</label>
-                        <input id="loginPassword" name="loginPassword" type="password" placeholder="your password" required>
+                        <input id="loginPassword" name="loginPassword" type="password" placeholder="your password"
+                            required>
                     </p>
 
                     <button type="submit" name="loginButton">
@@ -73,28 +76,33 @@ function getInputValue($name)
                         <?php echo $account->getError(Constants::$usernameCharacters); ?>
                         <?php echo $account->getError(Constants::$usernameTaken); ?>
                         <label for="username">Username</label>
-                        <input id="username" name="username" type="text" placeholder="name" value="<?php getInputValue('username') ?>" required>
+                        <input id="username" name="username" type="text" placeholder="name"
+                            value="<?php getInputValue('username') ?>" required>
                     </p>
                     <p>
                         <?php echo $account->getError(Constants::$firstNameCharacters); ?>
                         <label for="firstName">First name</label>
-                        <input id="firstName" name="firstName" type="text" placeholder="first name" value="<?php getInputValue('firstName') ?>" required>
+                        <input id="firstName" name="firstName" type="text" placeholder="first name"
+                            value="<?php getInputValue('firstName') ?>" required>
                     </p>
                     <p>
                         <?php echo $account->getError(Constants::$lastNameCharacters); ?>
                         <label for="lastName">Last Name</label>
-                        <input id="lastName" name="lastName" type="text" placeholder="last name" value="<?php getInputValue('lastName') ?>" required>
+                        <input id="lastName" name="lastName" type="text" placeholder="last name"
+                            value="<?php getInputValue('lastName') ?>" required>
                     </p>
                     <p>
                         <?php echo $account->getError(Constants::$emailInvalid); ?>
                         <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
                         <?php echo $account->getError(Constants::$emailTaken); ?>
                         <label for="email">Email</label>
-                        <input id="email" name="email" type="email" placeholder="e.g. email@gmail.com" value="<?php getInputValue('email') ?>" required>
+                        <input id="email" name="email" type="email" placeholder="e.g. email@gmail.com"
+                            value="<?php getInputValue('email') ?>" required>
                     </p>
                     <p>
                         <label for="email2">Confirm email</label>
-                        <input id="email2" name="email2" type="email" placeholder="e.g. email@gmail.com" value="<?php getInputValue('email2') ?>" required>
+                        <input id="email2" name="email2" type="email" placeholder="e.g. email@gmail.com"
+                            value="<?php getInputValue('email2') ?>" required>
                     </p>
                     <p>
                         <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
